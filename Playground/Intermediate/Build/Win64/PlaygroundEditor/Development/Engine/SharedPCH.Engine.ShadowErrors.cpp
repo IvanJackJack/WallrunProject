323c107af5ab01b0e7e7157795e6bcea102c2c45
IvanJackJack <ivancimino@hotmail.it>
2022-07-14 10:06:34 +0200

    reset repo

--- Playground/Intermediate/Build/Win64/PlaygroundEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.30.30711
-#include "C:/Users/ivanc/Desktop/Git repos/Unreal Repos/WallrunProject/Playground/Intermediate/Build/Win64/PlaygroundEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- Playground/Source/Playground/Playground.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Playground.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Playground, "Playground" );
